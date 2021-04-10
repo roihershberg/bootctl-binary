5cf05ee1aa26cc84ca13766382525066958e175a
roihershberg <roihershberg@gmail.com>
2021-04-10 16:51:51 +0300

    Update customize.sh
    Changed the permission of bootctl from 755 to 750.

--- customize.sh
+++ customize.sh
@@ -58,7 +58,7 @@ set_permissions() {
   # set_perm $MODPATH/system/lib/libart.so 0 0 0644
   # set_perm /data/local/tmp/file.txt 0 0 644
   
-  set_perm $MODPATH/system/xbin/bootctl 0 0 755
+  set_perm $MODPATH/system/xbin/bootctl 0 0 750
 }
 
 ##########################################################################################
